1ff66e2b3776af094aad02c98b07853eb43c8ed2
Alibi-stack <afox01155@gmail.com>
2024-12-30 22:14:21 +0500

    first commit

---
+++ src/models/Person.java
@@ -0,0 +1,21 @@
+package models;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String gender;
+
+    public Person(String name, String surname, int age, boolean isMale) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = isMale ? "Male" : "Female";
+    }
+
+    public String toString() {
+        return "Hi, I am " + name + " " + surname + ", a " + age + "-year-old " + gender + ".";
+    }
+}
+
+
